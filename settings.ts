4604fce930089e46644b5aec1e930d8763c39fe0
lose-dows <k413b0171n@gmail.com>
2025-02-14 07:00:41 -0800

    settings.ts

---
+++ settings.ts
@@ -0,0 +1,5 @@
+export const API_URL = "https://api.memoryassist.com";
+export const DEFAULT_REMINDER_TIME = "09:00"; // Default time for daily reminders
+export const MAX_REMINDERS = 10; // Maximum number of reminders a user can set
+export const EMERGENCY_CONTACTS_LIMIT = 5; // Maximum number of emergency contacts
+export const APP_VERSION = "1.0.0"; // Current version of the application
